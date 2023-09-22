95b631ab1711004b9d411363e5a3599e13533019
CodeWithYash99 <yashwanthrangu99@gmail.com>
2023-09-22 17:01:25 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { Button, Container, Row } from 'react-bootstrap'
+import { ClipLoader } from 'react-spinners'
 
-function App() {
+import './App.css'
+
+const App = () => {
+  const [number, setNumber] = useState('')
+  const [loader, setLoader] = useState(true)
+  const [text, setText] = useState('0 is the atomic number of the theoretical element tetraneutron.')
+
+  useEffect(() => {
+    setLoader(false) }, [])
+
+  const onChangeInput = async (event) => {  
+    setNumber(event.target.value)
+  }
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    let url = "https://apis.ccbp.in/numbers-fact?number=" + number;
+    let options = {
+        method: "GET"
+    };
+    const getData = await fetch(url, options)
+    const data = await getData.json()
+
+    const { fact } = data
+    setText(fact)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid>
+      <Row>
+        <div className='app-container d-flex flex-column justify-content-center'>
+          <h1 className='app-title mb-4'>Enter a number to know interesting facts about the number</h1>
+          <form className='form-container' onSubmit={onSubmitHandler}>
+            <input 
+              className='user-input'
+              type='text'
+              name='text'   
+              placeholder='Enter your Number...' 
+              value={number} 
+              onChange={onChangeInput}
+            />
+            <Button type='submit'>Search</Button>
+          </form>
+
+          {loader ? 
+            <><ClipLoader color='#00bfff' width={50} aria-label="Loading Spinner" data-testid="loader" /></> 
+            : 
+            <><p className="fact-text mt-4">{text}</p></>
+          }
+        </div>
+      </Row>
+    </Container>
+  )
 }
 
-export default App;
+export default App
